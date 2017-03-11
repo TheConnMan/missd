3e7d494bde31f9d9ba6bba1e83ffae1dd16fd712
TheConnMan <bcconn2112@gmail.com>
2017-03-10 20:23:10 -0500

    Add dedicated landing page

--- api/controllers/HomeController.js
+++ api/controllers/HomeController.js
@@ -1,10 +1,15 @@
 module.exports = {
   index: function(req, res) {
-    res.view({
+    var data = {
       user: req.user,
       github: sails.config.oauth.github.clientID && sails.config.oauth.github.clientSecret,
       google: sails.config.oauth.google.clientID && sails.config.oauth.google.clientSecret,
       twitter: sails.config.oauth.twitter.consumerKey && sails.config.oauth.twitter.consumerSecret
-    });
+    };
+    if (req.user) {
+      res.view(data);
+    } else {
+      res.view('home/landing', data);
+    }
   }
 };
