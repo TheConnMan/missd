833372b59ab10fe3647484c46307c10c750b2ced
TheConnMan <bcconn2112@gmail.com>
2017-03-11 16:15:24 -0500

    Add job actions to cards

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,16 @@
 angular
-.module('app', ['ngResource'])
+.module('app', ['ngResource', 'angularMoment'])
 .controller('controller', function($scope, $http, $resource) {
 
   var Job = $resource('/jobs/:id');
 
   $scope.jobs = Job.query();
+
+  $scope.getTime = function(date) {
+    return new Date(date).getTime();
+  };
+
+  $scope.formatDuration = function(duration) {
+    return moment.duration(duration).format();
+  };
 });
