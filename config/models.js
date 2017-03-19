579df086c9ea8d04c083722d54949ab8c34240b5
TheConnMan <bcconn2112@gmail.com>
2017-03-19 15:40:25 -0400

    Add sails persistence logger for ORM logging

--- config/models.js
+++ config/models.js
@@ -1,5 +1,11 @@
+var sailsLogger = require('sails-persistence-logger')();
+
 module.exports.models = {
   connection: process.env.MYSQL_HOST ? 'mysql' : 'localDiskDb',
 
-  migrate: 'alter'
+  migrate: 'alter',
+
+  afterCreate: sailsLogger.afterCreate,
+  afterUpdate: sailsLogger.afterUpdate,
+  afterDestroy: sailsLogger.afterDestroy
 };
