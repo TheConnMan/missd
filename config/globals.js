dd4fca4fabcc6ce1d43e8d769732f52a86c712dc
TheConnMan <bcconn2112@gmail.com>
2017-06-04 13:32:27 -0400

    Show commit hash in non-master builds and link to the GitHub commit

--- config/globals.js
+++ config/globals.js
@@ -1,5 +1,10 @@
+var REPO_ROOT = 'https://github.com/TheConnMan/missd/';
+var version = process.env.npm_package_version;
+var commit = (version || '').split('-').length === 1 ? null : version.split('-')[1].slice(0, 7);
+
 module.exports.globals = {
-  version: process.env.npm_package_version,
+  version: commit ? version.split('-')[0] + '-' + commit : version,
+  versionLink: commit ? REPO_ROOT + 'commit/' + commit : REPO_ROOT + 'releases/tag/v' + version,
   proxy: process.env.PROXY_URL || 'http://localhost:4200',
   proxyEnabled: process.env.PROXY_ENABLED === undefined ? false : process.env.PROXY_ENABLED === 'true',
 
